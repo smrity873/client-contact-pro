c72f255cc8abe0fd76aec5c720ad8a596a9fc5da
rhmunna143 <rhmunna19@gmail.com>
2024-09-12 11:52:03 +0600

    folder structure added

---
+++ src/app/(root)/contacts/[contactId]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            single contact  
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(root)/contacts/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            all contacts
+        </div>
+    );
+};
+
+export default page;
