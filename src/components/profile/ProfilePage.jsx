28c267ccfee26b0de4315ee2561f65d89749d07c
rhmunna143 <rhmunna19@gmail.com>
2024-09-27 22:22:19 +0600

    no user dp exception handled

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -4,7 +4,9 @@ import useUserProfile from '@/hooks/useUserProfile';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { FaRegEdit } from 'react-icons/fa';
+import { FaRegEdit, FaUserCircle } from 'react-icons/fa';
+
+// userProfile?.profile_picture_url &&
 
 const ProfilePage = () => {
     const { userProfile } = useUserProfile();          // Custom hook to get user profile data
@@ -14,8 +16,14 @@ const ProfilePage = () => {
             <h2 className='font-medium'>Profile</h2>
 
             <div className="profile w-fit mx-auto space-y-4 mt-10">
-                <div className="profile__picture h-20 w-20 aspect-square rounded-full border border-primary mx-auto">
-                    <Image src={userProfile?.profile_picture_url} priority={false} alt={"dp"} height={80} width={80} className='aspect-square rounded-full' />
+                <div className="profile__picture h-20 w-20 aspect-square rounded-full border border-primary mx-auto flex justify-center items-center">
+                    {
+                        userProfile?.profile_picture_url && (<Image src={userProfile?.profile_picture_url} priority={false} alt={"dp"} height={80} width={80} className='aspect-square rounded-full' />)
+                    }
+
+                    {
+                        !userProfile?.profile_picture_url && (<FaUserCircle className='text-7xl text-primary' />)
+                    }
                 </div>
 
                 <div className="profile__info space-y-2">
